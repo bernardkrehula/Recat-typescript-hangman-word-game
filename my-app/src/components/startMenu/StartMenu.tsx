2abb41c839fe8ed45a4cb49ad85b52bf410d9810
Bernard Krehula <krehula.bernard456@gmail.com>
2025-09-10 22:58:01 +0200

    display modes

--- my-app/src/components/startMenu/StartMenu.tsx
+++ my-app/src/components/startMenu/StartMenu.tsx
@@ -9,18 +9,19 @@ type HangmanMode = {
 const StartMenu = ({hangman}: HangmanMode) => {
     
     
-    const displayData = () => {
-        console.log(hangman)
-        console.log(Object.keys(hangman.mode))
+    const displayModes = () => {
+       return Object.keys(hangman.mode).map(mode => (<Btn key={mode}>{mode}</Btn>));
+    }
+    
+    const displayCategories = () => {
+
     }
     return(
         <div className='overlay'>
             <div className="start-menu">
                 <h2>Chose mode:</h2>
                 <div className='easy-hard-btns'>
-                    {/* {Object.keys(hangman.mode).map()} */}
-                    <Btn onClick={displayData}>Easy</Btn>
-                    <Btn>Hard</Btn>
+                    {displayModes()}
                 </div>
                 <h2>Chose Category to start the game:</h2>
                 <div className='categories'>
